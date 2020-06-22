cc654b7b3d828bb9345686085323aced8321b1d4
Jessica Seo <jessicaseo83@gmail.com>
2020-06-22 22:20:08 +0000

    BootcampX Queries 3 - GROUP BY

---
+++ 3_queries_group-by/total_assignment_submissions.sql
@@ -0,0 +1,6 @@
+SELECT cohorts.name AS cohort, COUNT(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+JOIN students ON students.id = assignment_submissions.student_id
+JOIN cohorts ON cohorts.id = students.cohort_id
+GROUP BY cohort
+ORDER BY total_submissions DESC;
