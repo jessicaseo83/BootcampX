75a221d8bf41ecfcb6c8de2a2012d683cb655788
Jessica Seo <jessicaseo83@gmail.com>
2020-06-23 17:19:39 +0000

    Modified COUNT

--- 3_queries_group-by/total_assignment_submissions.sql
+++ 3_queries_group-by/total_assignment_submissions.sql
@@ -1,4 +1,4 @@
-SELECT cohorts.name AS cohort, COUNT(assignment_submissions.*) as total_submissions
+SELECT cohorts.name AS cohort, COUNT(assignment_submissions) as total_submissions
 FROM assignment_submissions
 JOIN students ON students.id = assignment_submissions.student_id
 JOIN cohorts ON cohorts.id = students.cohort_id
